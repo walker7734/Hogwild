1d15680eaa9fd65d7261540e2d52cd7e5d68fe6a
David Leen <dleen@uw.edu>
2013-02-23 20:49:28 -0800

    fixing

--- src/main/java/hogwild_abstract/HogwildDataInstance.java
+++
@@ -1,7 +0,0 @@
-package hogwild_abstract;
-
-public abstract class HogwildDataInstance {
-    
-    public abstract String toString();
-    public abstract int getLabel();
-}
--- src/main/java/hogwild_abstract/HogwildWeights.java
+++
@@ -1,7 +0,0 @@
-package hogwild_abstract;
-
-public abstract class HogwildWeights {
-    public abstract int l0norm();
-    public abstract double l2norm();
-
-}
